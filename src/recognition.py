8877b80bf67e7bfd3db99f438e0986c32a69a40e
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-04-16 23:28:34 -0700

    adding recognition

---
+++ src/recognition.py
@@ -0,0 +1,40 @@
+import face_recognition
+import os
+import pandas as pd
+import numpy as np
+from glob import glob
+from sklearn.ensemble import RandomForestClassifier
+
+
+data = []
+target = []
+face_names = ['donald_trump', 'mike_pence', 'putin']
+i = 0
+for file_path in glob.glob(os.environ.get("INPUT_DIR")+"/*/*"):
+    print i
+    i+=1
+    load_image = face_recognition.load_image_file(file_path)
+    face_encoding = face_recognition.face_encodings(load_image)[0]
+    data.append(face_encoding)
+    class_name = file_path.split('/')[-1]
+    target.append(face_names.index(class_name))
+
+
+data = np.asarray(data)
+dimensions = range(len(face_encoding))
+
+df = pd.DataFrame(data, columns=face_encoding)
+df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+df['face'] = pd.Categorical.from_codes(target, face_names)
+df.head()
+
+train, test = df[df['is_train']==True], df[df['is_train']==False]
+
+features = df.columns[:len(face_encoding)]
+clf = RandomForestClassifier(n_jobs=4)
+y, _ = pd.factorize(train['species'])
+clf.fit(train[features], y)
+
+
+preds = face_names[clf.predict(test[features])]
+pd.crosstab(test['face'], preds, rownames=['actual'], colnames=['preds'])
