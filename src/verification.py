0568cac35225767c07ecac5f5fdf0406efd4a6ea
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-04-27 07:00:06 -0700

    adding ipynb file

--- src/verification.py
+++ src/verification.py
@@ -2,9 +2,9 @@ import face_recognition
 import os
 
 donald_image_path = os.path.join(os.environ.get("INPUT_DIR"), 'donald_trump', '1.jpg')
-unknow_image_path = os.path.join(os.environ.get("INPUT_DIR"), 'test', 'test_image.jpg')
+unknown_image_path = os.path.join(os.environ.get("INPUT_DIR"), 'test', 'test_image.jpg')
 known_image = face_recognition.load_image_file(donald_image_path)
-unknown_image = face_recognition.load_image_file(unknow_image_path)
+unknown_image = face_recognition.load_image_file(unknown_image_path)
 
 donald_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
