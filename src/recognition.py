452b1cad86d080aaa887dbe8e221d34609dc94ef
shabazbpatel <shabaz.b.patel@gmail.com>
2017-05-22 07:09:47 +0000

    adding recognition.py for train command

--- src/recognition.py
+++ src/recognition.py
@@ -2,21 +2,31 @@ import face_recognition
 import os
 import pandas as pd
 import numpy as np
+import json
 from glob import glob
 from sklearn.ensemble import RandomForestClassifier
 from scikit_checkpoint import ScikitCheckpoint
 from sklearn.cluster import KMeans
+from sklearn.metrics import precision_recall_fscore_support
 np.random.seed(5)
 
 data = []
 target = []
 face_names = []
 num_faces = len(face_names)
+# Training load data
 faces_selected = set()
+
+with open(os.environ['INPUT_DIR']+'/config.json') as f:
+     config = json.load(f)
+
+class_names = config['class_names']
+n_jobs = int(config['n_jobs'])
+split_prob = config['split_prob']
 
 for file_path in glob(os.environ.get("DATA_DIR")+"/*/*"):
     class_name = file_path.split('/')[-2]
-    if not class_name == 'test':
+    if class_name in class_names and class_name != 'test':
         if class_name not in faces_selected:
             faces_selected.add(class_name)
             face_names.append(class_name)
@@ -32,21 +42,30 @@ data = np.asarray(data)
 dimensions = range(len(face_encoding))
 
 df = pd.DataFrame(data, columns=dimensions)
-df['is_train'] = np.random.uniform(0, 1, len(df)) <= .60
+df['is_train'] = np.random.uniform(0, 1, len(df)) <= split_prob
 df['face'] = pd.Categorical.from_codes(target, face_names)
 df.head()
 
 train, test = df[df['is_train']==True], df[df['is_train']==False]
 
 features = df.columns[:len(face_encoding)]
-clf = RandomForestClassifier(n_jobs=4)
+clf = RandomForestClassifier(n_jobs=n_jobs)
 y, _ = pd.factorize(train['face'])
 clf.fit(train[features], y)
 checkpoint = ScikitCheckpoint(os.environ['SNAPSHOTS_DIR'], )
-stats = {'label': 'random_forest'}
 
-checkpoint.save_model(clf, stats)
 preds = face_names[np.array(clf.predict(test[features]))]
-preds = face_names[clf.predict(test[features])]
 cross_validation = pd.crosstab(test['face'], preds, rownames=['actual'], colnames=['preds'])
-print(cross_validation)
+y_true = np.array(list(test['face']))
+y_pred = np.array(preds)
+
+p_r_f1 = precision_recall_fscore_support(y_true, y_pred, average='macro')
+precision = p_r_f1[0]
+recall = p_r_f1[1]
+f1_score = p_r_f1[2]
+stats = {'label': 'random_forest'}
+stats['precision'] = precision
+stats['recall'] = recall
+stats['f1_score'] = f1_score
+print stats
+checkpoint.save_model(clf, stats)
