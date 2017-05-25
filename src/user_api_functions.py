917bef417a698c18146e7d2fd49643fd8138efb0
shabazbpatel <shabaz.b.patel@gmail.com>
2017-05-25 01:05:10 +0000

    updating code for multiple classes

--- src/user_api_functions.py
+++ src/user_api_functions.py
@@ -4,9 +4,13 @@ import pickle
 import os
 import numpy as np
 import face_recognition
+import json
+
 
 filename = os.path.join(os.environ['INPUT_DIR'],'model.dat')
 clf = pickle.load( open(filename , "rb" ) )
+filename = os.path.join(os.environ['SHARED_OUTPUT_DIR'],'face_names.pkl')
+face_names = np.array(pickle.load(open(filename, 'rb')))
 
 def add(params):
     return params['a'] + params['b']
@@ -23,16 +27,9 @@ def recognition(params):
     # read the image file in a numpy array
     list_encoding = face_recognition.face_encodings(image)
     test_pred = []
-    face_names = np.array(['other', 'donald_trump'])
     test_preds = []
     if list_encoding:
         for encoding in list_encoding:
-            proba = clf.predict_proba([encoding]).reshape(1, -1)
-            proba = proba.reshape(1, -1)
-            if proba[0][1] >= 0.7:
-                test_pred = face_names[1]
-                test_preds.append(test_pred)                
-            else:
-                test_pred = face_names[0]
-                test_preds.append(test_pred)                
-    return test_preds
+            test_pred = face_names[clf.predict([encoding])][0]
+            test_preds.append(test_pred)
+    return list(test_preds)
